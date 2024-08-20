b2121ceb82a0625ac80e96e3eb3a5efb903acdae
hoangminh-tech <Hoangminh91200085@gmail.com>
2024-08-20 15:09:49 +0700

    Buổi 6 (Mảng)

--- src/Array/Array2.java
+++ src/Array/Array2.java
@@ -4,13 +4,13 @@ public class Array2
 {
     public static void main(String[] args) {
         //khai báo và gan gia tri cho mảng
-//        int number2[]={5,10,15,20,0};
-//
-//        number2[4]=100;
-//        System.out.println("kích cỡ mảng: " +number2.length);
-//        for (int i =0; i<number2.length;i++){
-//            System.out.println(number2[i]);
-//        }
+        int number2[]={5,10,15,20,0};
+        boolean check = true;
+        number2[4]=100;
+        System.out.println("kích cỡ mảng: " +number2.length);
+        for (int i =0; i<number2.length;i++){
+            System.out.println(number2[i]);
+        }
 
         //Ví dụ 2: AUTOMATION
         String Customer [] = {"Hoa", "Lan", "Huệ", "Cúc", "Thủy"};
@@ -18,13 +18,31 @@ public class Array2
         System.out.println(Customer[1]);//Test lần 2
         System.out.println(Customer[2]);//Test lần 3
         System.out.println(Customer[3]);//Test lần 4
+        System.out.println("====================================");
 
+        // Dùng For kiểm tra theo tên
+        for (int i =0; i<Customer.length; i++){
+            if(Customer[i].equals("Lan"))
+            {
+                check = true;
+                break;
+            }else
+            {
+                check = false;
+            }
+        }
 
-        int CustomerAge [] = {"21", "22", "32", "23", "34"};
-        System.out.println(CustomerAge[0]);//Test lần 1
-        System.out.println(CustomerAge[1]);//Test lần 2
-        System.out.println(CustomerAge[2]);//Test lần 3
-        System.out.println(CustomerAge[3]);//Test lần 4
+        if(check == true){
+            System.out.println("Đã tìm thấy");
+        }else {
+            System.out.println("Không tìm thấy");
+        }
+
+//        int CustomerAge [] = {"21", "22", "32", "23", "34"};
+//        System.out.println(CustomerAge[0]);//Test lần 1
+//        System.out.println(CustomerAge[1]);//Test lần 2
+//        System.out.println(CustomerAge[2]);//Test lần 3
+//        System.out.println(CustomerAge[3]);//Test lần 4
 
     }
 }
